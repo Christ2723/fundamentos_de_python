05a6164ddcb767c99e39297f98219343c23db374
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-03-25 14:06:42 +0000

    ejercicio 11

--- ejercicios/ejercicio10.py
+++ ejercicios/ejercicio10.py
@@ -1,12 +1,29 @@
-# Recrear el Sign Up de GitHub.
+# Recrear el Sign Up de GitHub (Esta fue mi manera).
 
 usuario = input("Ingrese su usuario: ")
 contrasena = input("Ingrese la Contrasena: ")
-if usuario == usuario:
+if usuario 
     print("El usuario es correcto") 
 else:
     print("El usuario es incorrecto")       
-if contrasena == contrasena:
+if contrasena 
         print("La contrasena es correcta ")
 else:
     print("La contrasena es incorrecta")
+
+# La manera del maestro (Esto era lo que teniamos que hacer).
+
+import pwinput
+import time
+
+texto = "Welcome to GitHub! Let's begin the adventure"
+
+for letra in texto:
+  print(letra,end="",flush=True)
+  time.sleep(0.04)
+
+print("Enter your email")
+email = input("-> ")
+
+print("Create a password")
+input = pwinput.pwinput(mask="*")
