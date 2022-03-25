5f1edc6a41f39dfe9cb86181e7ef84c7a4285799
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-03-25 03:03:02 +0000

    ejercicio 10

---
+++ ejercicios/ejercicio10.py
@@ -0,0 +1,12 @@
+# Recrear el Sign Up de GitHub.
+
+usuario = input("Ingrese su usuario: ")
+contrasena = input("Ingrese la Contrasena: ")
+if usuario == usuario:
+    print("El usuario es correcto") 
+else:
+    print("El usuario es incorrecto")       
+if contrasena == contrasena:
+        print("La contrasena es correcta ")
+else:
+    print("La contrasena es incorrecta")
