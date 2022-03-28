e59f5c43bd02c991983a1f13889ebc936d9d1c43
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-03-28 15:24:10 +0000

    ternario

---
+++ ejercicios/ternario.py
@@ -0,0 +1,16 @@
+# Hacer un programa que al digitar una edad de una persona me diga si es mayor o menor de edad.
+
+edad = int(input("Digite su edad: "))
+
+if edad >= 18:
+    print("Usted es mayor de edad: ")
+else:
+    print("Usted no es mayor de edad:")  
+
+# Decir si un numero es positivo o negativo.
+
+num = int(input("Digite un numero para saber si es (t/f)")) 
+if num > 0:
+    print("positivo")
+else:
+    print("negativo")    
