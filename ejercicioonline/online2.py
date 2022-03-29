3166ff703a6d372d77583eddd2019c0c9fa2c55d
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-03-29 15:10:42 +0000

    tarea contar caracteres y presente los numeros de los caracteres

--- ejercicioonline/online2.py
+++ ejercicioonline/online2.py
@@ -1,2 +1,15 @@
 # Print characters from a string that are present at an even index number
 
+given_string = input('Enter a string: ')
+
+even_chars = []
+odd_chars = []
+
+for i in range(len(given_string)):
+    if i % 2 == 0:
+        even_chars.append(given_string[i])
+    else:
+        odd_chars.append(given_string[i])
+
+print('Odd characters: {}'.format(odd_chars))
+print('Even characters: {}'.format(even_chars))
