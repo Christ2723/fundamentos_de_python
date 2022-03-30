008e269cd4926e3d0149026b1e8e7f2d8797dcf0
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-03-30 15:42:37 +0000

    christ

--- ejercicios/tipos_iterables.py
+++ ejercicios/tipos_iterables.py
@@ -2,8 +2,20 @@
 
 # Sin isar if, mostrar el dia de la semana qie el usuario digite.
 
-semana = [ ]
+#semana = [ ]
 
-dias= input("Digite los dias de la semana: ")
-semana.append(dias)
-print("Los dias de la semana son: ",semana)
+#dias= input("Digite los dias de la semana: ")
+#semana.append(dias)
+#print("Los dias de la semana son: ",semana)
+
+# Digitar los alumnos de una clase con si respectiva nota (4 notas) y moestrar el promedio de cada alumno.
+lista = ()
+
+alumnos = input("Digites los alumnos: ")
+n1 = int(input("Digite primera nota: "))
+n2 = int(input("Digite segunda nota: "))
+n3 = int(input("Digite tercera nota: "))
+n4 = int(input("Digite cuarta nota: "))
+promedio = (n1+n2+n3+n4)/3
+print(promedio)
+
