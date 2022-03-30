8ac44b95555e9f68e8db814ac34547158f6d8ca3
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-03-30 15:18:45 +0000

    Clase de ayer

---
+++ ejercicios/rangos.py
@@ -0,0 +1,15 @@
+rango = range(10)
+print(rango)
+
+for valor in range:
+  print(valor)
+
+rango2 = range(5,15)
+print(rango2)
+
+for valor in range2:
+  print(valor)
+
+rango3 = range(2,20,3)
+print(rango3)
+
---
+++ ejercicios/tipos_iterables.py
@@ -0,0 +1,9 @@
+# Agragar los dias de la semana que el usuario digite y mostrarlos al final.
+
+# Sin isar if, mostrar el dia de la semana qie el usuario digite.
+
+semana = [ ]
+
+dias= input("Digite los dias de la semana: ")
+semana.append(dias)
+print("Los dias de la semana son: ",semana)
