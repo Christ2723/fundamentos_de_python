03a081d278d5f84bab198832d17f862dd121ee7f
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-04-01 13:11:17 +0000

    christ23

--- ejercicios/API/github.py
+++ ejercicios/API/github.py
@@ -1,11 +1,16 @@
 # Buscar un usuarui en github, mostrar su nombre, followes, following y sus repos publicos.
 
 #algoritmo(pseudocodigo):
-# validar (Que no este vacio.(buscar en python como saber cuando un texto esta vacio.(pedirle al usuarioque lo digite de nuevo(eliminar los espacios en blanco y repetir de nuevo la pregunta)utilizar una expresion regular para validar el patron de usuario de github.)vuelva a digitar el usuario, porque no se  completan caracteres especiales, digitos permitidos. letras de la a-zA-Z0-9)
+# validar (Que no este vacio.(buscar en python como saber cuando un texto esta vacio.(pedirle al usuario que lo digite de nuevo(eliminar los espacios en blanco y repetir de nuevo la pregunta)utilizar una expresion regular para validar el patron de usuario de github.)vuelva a digitar el usuario, porque no se  completan caracteres especiales, digitos permitidos. letras de la a-zA-Z0-9)
 # (usar la api) como usar una expresion regular en python?
 # si usuario encontrado: -mostrar su nombre,followers,following,public repos
 # de lo contrario: el usuario no existe, no fue encontrado. - desea crear un usuario? (y/n)
 # -si es: y (abrir en el navegador, la url para registrarse(https://github.com/signup) - si es : n (despedirse del usuario(hasta luego #Keepcoding)))
+
+nombre = input("Digite su nombre: ").strip().replace(" ","")
+while nombre == " ":
+  nombre = input("Digite su nombre: ").strip().replace(" ","")
+
 
 
 
