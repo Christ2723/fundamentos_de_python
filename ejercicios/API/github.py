edcf4c4703c17cb58daec474e3bf65fff3a52fae
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-04-01 13:00:18 +0000

    christ23

--- ejercicios/API/github.py
+++ ejercicios/API/github.py
@@ -1,11 +1,14 @@
-import re
-usuario = input("Digite el usuario: ")
+# Buscar un usuarui en github, mostrar su nombre, followes, following y sus repos publicos.
 
-patron = " Ʌ[a-zA-Z0-9]{3,25}$"
-resultado = re.search(patron, usuario)
+#algoritmo(pseudocodigo):
+# validar (Que no este vacio.(buscar en python como saber cuando un texto esta vacio.(pedirle al usuarioque lo digite de nuevo(eliminar los espacios en blanco y repetir de nuevo la pregunta)utilizar una expresion regular para validar el patron de usuario de github.)vuelva a digitar el usuario, porque no se  completan caracteres especiales, digitos permitidos. letras de la a-zA-Z0-9)
+# (usar la api) como usar una expresion regular en python?
+# si usuario encontrado: -mostrar su nombre,followers,following,public repos
+# de lo contrario: el usuario no existe, no fue encontrado. - desea crear un usuario? (y/n)
+# -si es: y (abrir en el navegador, la url para registrarse(https://github.com/signup) - si es : n (despedirse del usuario(hasta luego #Keepcoding)))
 
-if resultado:
-  print("Correcto")
-else:
-  print("Intente de nuevo")
+
+
+  
+
 
--- ejercicios/christapi.py
+++ ejercicios/christapi.py
@@ -1,36 +1,6 @@
-import requests  
+import requests 
 
-expresion = [a-z,A-Z,0-9]
 
-usuario = input("Digite su usuario: ").strip().replace(" ","").lower()
-if usuario not in expresion:
-  print("Debe digitar su usuario de nuevo: ")
-elif usuario in expresion:
-  print("espere....") 
-URL = https://github.com/search?q=&type=
-data = requests.get(URL) 
-
-data = data.json() 
-
-for element in data[()]:
-    print(element['usuario encontrado'])
-
-followers = 452
-followings = 275
-prublic_repos = 5
-
-if element == "usuario encontrado":
-  print(usuario,followers,followings,public_repos)
-else:
-  print("El usuario no existe, no fue encontrado. ")
-https://github.com/session
-encontrado = input("Desea crear un usuario: (y/n)?")
-if encontrado == "y":
-  print("Abrir en el navegador, la URL para registrarse: https://github.com/session")
-
-if encontrado == "n":
-  print("Hasta luego. #KeepCoding.")
-  
   
   
   
