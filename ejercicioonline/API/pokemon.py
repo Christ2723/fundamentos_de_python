6ceb18cf2d18674d59e2d09de6df238d9910d1fe
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-04-01 15:07:10 +0000

    christ23

--- ejercicioonline/API/pokemon.py
+++ ejercicioonline/API/pokemon.py
@@ -8,6 +8,24 @@ url = f" https://pokeapi.co/api/v2/ability/{usuario}"
 respuesta = requests.get(url)
 if respuesta:
   datos = respuesta.json()
-  print(datos)
+  print("Nombres:")
+  print(f"Cantidad de nombres: {len(datos['name']) }")
+
+  idiomas = []
+  habilidades = []
+  for nombres in datos['names']:
+    idiomas.append(nombres['language']['name'])
+    habilidades.append(nombres,['name'])
+
+  print("-"*20)
+  print("Listado de idiomas: ")
+  for idioma in idiomas:
+    print(idioma)
+
+  print("Listado de habilidades: ")
+  for habilidades in habilidades:
+    print(habilidad)
 else:
   Print("El pokemo no existe: ")
+
+
