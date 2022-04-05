420d5aea8b9aefd384c2a85d8e929454e1b49ea2
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-04-05 14:28:29 +0000

    christ23

--- ejercicioonline/pokemon.py
+++ ejercicioonline/pokemon.py
@@ -1,3 +1,13 @@
+# Hacer un programa que muestre un listado con los pokemones, el usuario tendra la oportunidad de digitar su id correspondeinte, y se le mostrara la informacion relacionada al mismo.
+
+# ademas, el usuario podra navegar en el programa mediante los shortcuts y/o teclas del teclado:
+# algunas de las opciones serian:
+# ver siguiente/anterior listado:
+# ver siguiente/anterior pokemo:
+# salir del programa:
+# buscar un pokemon por su id:
+# buscar:
+
 import re
 import requests
 import webbrowser
