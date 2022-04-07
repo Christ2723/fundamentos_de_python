6972dcab5dcc074acc534d94c7768dd755776cef
Christopher Payero <102038297+Christ2723@users.noreply.github.com>
2022-04-07 13:39:02 +0000

    christ23

--- ejercicios/reto_pokemon.py
+++ ejercicios/reto_pokemon.py
@@ -1,13 +1,15 @@
 import requests
 import re
-# Constantes
+# Constantes    
 base_url = "https://pokeapi.co/api/v2/"
 regrex_numbers = "[0-9]{1,3}$"
 
 limite = input("Digite la cantidad de pokemos a mostrar: ")
 
-while not re.search(regrex_numbers, limite):
-    limite = input("Digite la cantidad de pokemones a mostrar: ")
+while(not re.search(regrex_numbers,id_digitado)) or (id_digitado < primero or id_digitado > ultimo):
+    id_digitado = input(f"por favor, digite un id valido [{primero}-{ultimo}]:")
+
+id_digitado = int(id_digitado)
 
 print("Mostrar primeros {limite} pokemones:")
 
@@ -17,88 +19,8 @@ respuesta = requests.get(url)
 if respuesta:
     datos = respuesta.json()
 
-    primero = Nimport requests
-import re
-￼
-print("Mostrar primeros 20 pokemones:")
-￼
-base_url = "https://pokeapi.co/api/v2/"
-regrex_numbers = "[0-9]{1,3}$"
-￼
-url = base_url + "pokemon?limit=100&offset=200"
-respuesta = requests.get(url)
-￼
-if respuesta:
-    datos = respuesta.json()
-    ￼
     primero = None
     ultimo = None
-    for pokemon in datos ['results']:
-        id_pokemon = pokemon['url'].split('/')[6]
-        nombre = pokemon['name']
-      ￼
-    if primero == None:
-        primero = int(id_pokemon)
-      ￼
-    print(id_pokemon, nombre)
-  ￼
-ultimo = int(id_pokemon)
-￼
-id_digitado = input(f'Digite un id[{primero}-{ultimo}]:')
-resultado = re.search(regrex_numbers, id_digitado)
-import requests
-import re
-￼
-print("Mostrar primeros 20 pokemones:")
-￼
-base_url = "https://pokeapi.co/api/v2/"
-regrex_numbers = "[0-9]{1,3}$"
-￼
-url = base_url + "pokemon?limit=100&offset=200"
-respuesta = requests.get(url)
-￼
-if respuesta:
-    datos = respuesta.json()
-    ￼
-    primero = None
-    ultimo = None
-    for pokemon in datos ['results']:
-        id_pokemon = pokemon['url'].split('/')[6]
-        nombre = pokemon['name']
-      ￼
-    if primero == None:
-        primero = int(id_pokemon)
-      ￼
-    print(id_pokemon, nombre)
-  ￼
-ultimo = int(id_pokemon)
-￼
-id_digitado = input(f'Digite un id[{primero}-{ultimo}]:')
-resultado = re.search(regrex_numbers, id_digitado)
-￼
-while not resultado:
-    id_digitado = input(f'Digite un id [{primero}-{ultimo}]: ')
-    resultado = re.search(regrex_numbers,id_digitado)
-  ￼
-id_digitado = int(id_digitado)
-￼
-while id_digitado < primero or id_digitado > ultimo:
-    id_digitado = int(input(f'Digite un id valido[{primero}-{ultimo}]:'))
-  ￼
-else:
-    print(f"Ocurrio un error ({respuesta.status_code}) al obtener los datos")  
-while not resultado:
-    id_digitado = input(f'Digite un id [{primero}-{ultimo}]: ')
-    resultado = re.search(regrex_numbers,id_digitado)
-  ￼
-id_digitado = int(id_digitado)
-￼
-while id_digitado < primero or id_digitado > ultimo:
-    id_digitado = int(input(f'Digite un id valido[{primero}-{ultimo}]:'))
-  ￼
-else:
-    print(f"Ocurrio un error ({respuesta.status_code}) al obtener los datos") one
-    ultimo = None
 
     for pokemon in datos ['results']:
         id_pokemon = pokemon['url'].split('/')[6]
@@ -124,5 +46,5 @@ while id_digitado < primero or id_digitado > ultimo:
     id_digitado = int(input(f'Digite un id valido[{primero}-{ultimo}]:'))
 
 else:
-    print(f"Ocurrio un error ({respuesta.status_code}) al obtener los datos")     
+    print(f"Ocurrio un error ({respuesta.status_code}) al obtener los datos")    
          
